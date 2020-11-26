6883578523f07462148b3cb594c91fe492ab6e73
Kaan Oktay <kaan.oktay@boun.edu.tr>
2020-11-26 03:09:27 +0100

    Performance improvements.

--- seft_transformer/main.py
+++ seft_transformer/main.py
@@ -101,7 +101,7 @@ def main():
     # Callback for Tensorboard logging
     tensorboard_callback = keras.callbacks.TensorBoard(
         log_dir=logs_directory,
-        profile_batch='10, 15'
+        profile_batch='300, 305'
     )
 
     # Fit the model to the input data
