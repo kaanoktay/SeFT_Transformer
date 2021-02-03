ba3fc50ead517adc3f9eb8e83e16a16159c3c77e
Kaan Oktay <kaan.oktay@boun.edu.tr>
2021-02-03 01:19:48 +0100

    Relative position feature added.

--- seft_transformer/main.py
+++ seft_transformer/main.py
@@ -152,7 +152,7 @@ def main():
     tensorboard_callback = keras.callbacks.TensorBoard(
         log_dir=experiment_log,
         update_freq='epoch',
-        profile_batch='300,320'
+        profile_batch='100,120'
     )
 
     # Fit the model to the input data
